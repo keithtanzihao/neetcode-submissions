f7aa68adc044a7eb3a138f848e3e0abd3df7bb05
Keith Tan <67083090+keithtanzihao@users.noreply.github.com>
2026-09-01 16:49:06 +0800

    Time: 31 ms (5.91%) | Memory: 19.2 MB (55.22%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -6,17 +6,16 @@ class Solution:
         m = len(l) % 2
         h = len(l) // 2
 
-        for i in range(0, len(l)):
+        for i in range(0, h):
             v1 = l[i]
             v2 = l[c-i]
 
-            if m == 1 and i == h and c-i == h:
-                return l[i] == l[c-i]
-            if m == 0 and i == h-1 and c-i == h:
-                return l[i] == l[c-i]
+            print(i, v1, v2)
+
             if v1 != v2:
                 return False
-            
+        
+        return True
             
             
             
