c1efecd05df8cd7b84ffa3129a397d5b0a04eea7
Keith Tan <67083090+keithtanzihao@users.noreply.github.com>
2026-09-01 16:39:11 +0800

    Time: 40 ms (5.91%) | Memory: 19.5 MB (19.35%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        l = list(str(x))
+        c = len(l)-1
+
+        m = len(l) % 2
+        h = len(l) // 2
+
+        for i in range(0, len(l)):
+            v1 = l[i]
+            v2 = l[c-i]
+
+            print(i, v1, v2)
+
+            if m == 1 and i == h and c-i == h:
+                return l[i] == l[c-i]
+            if m == 0 and i == h-1 and c-i == h:
+                return l[i] == l[c-i]
+            if v1 != v2:
+                return False
+            
+            
+            
+            
