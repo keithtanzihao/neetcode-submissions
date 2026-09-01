a5a679ff5929472948722ec85cee1dea247acebb
Keith Tan <67083090+keithtanzihao@users.noreply.github.com>
2026-09-01 16:07:06 +0800

    Time: 1 ms (61.27%) | Memory: 20.4 MB (58.70%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,23 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-        result = []
-        store = {v: i for i, v in enumerate(nums)} 
-
+        store = {}
         for i in range(len(nums)):
-            val2 = target - nums[i]
-            
-            if val2 in nums and i != store[val2]:
-                return[i, store[val2]]
+            val2 = target - nums[i] # 3
 
+            if val2 in store and store[val2] != i:
+                return [store[val2], i]
             
+            store[nums[i]] = i # 3: 0
 
-        # Too slow i guess this is technically maybe 
-        # result = []
-        # for i in range(0, len(nums)-1):
-        #     for j in range(i+1, len(nums)):
-        #         if (nums[i] + nums[j]) == target:
-        #             return result.extend([i, j])
                     
             
             
