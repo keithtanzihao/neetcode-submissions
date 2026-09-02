ead1cbea045e0286c50c3b18a30655ec8e4d8f30
Keith Tan <67083090+keithtanzihao@users.noreply.github.com>
2026-09-02 16:21:59 +0800

    Time: 0 ms (100.00%) | Memory: 19.4 MB (23.63%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        map = {
+            ")": "(",
+            "}": "{",
+            "]": "["
+        }
+        l_a = ["(", "{", "["]
+        l = ""
+
+        if len(s)%2 != 0:
+            return False
+
+        for c in s:
+            if c in l_a:
+                l += c
+
+            if not l:
+                return False
+
+            if c in map:
+                if l[-1] == map[c]:
+                    l = l[:-1]
+                else:
+                    return False
+        
+        if not l:
+            return True
+        return False
+            
+
+                
