5fb65d4b5df3cdc97995c13d3dd8e465ae21183d
Keith Tan <67083090+keithtanzihao@users.noreply.github.com>
2026-09-13 23:57:13 +0800

    Time: 0 ms (100.00%) | Memory: 19.9 MB (44.18%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -7,18 +7,12 @@ class Solution:
             mid_pt = (s_pt + e_pt) // 2
             mid_val = nums[mid_pt]
 
-            print(f"Before s:{s_pt:<5} e:{e_pt:<5} mid P: {mid_pt:<5} mid V:{mid_val}")
-
             if mid_val == target:
                 return mid_pt
             elif mid_val > target:
                 e_pt = mid_pt - 1
             else:
                 s_pt = mid_pt + 1
-
-            print(f"After  s:{s_pt:<5} e:{e_pt:<5}")
-            print("\n")
-
 
         return s_pt
 
